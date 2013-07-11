27162a4f08c1a5eb322c1f3c0befd40f8c176cac
icedwater <icedwater@gmail.com>
2013-07-11 01:35:06 +0000

    Attempted URL check, needs debugging.

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://localhost/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +36,18 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var assertURLExists = function(url) {
+    loadedFile = restler.get(url).on('complete', function(result) {
+        if (result instanceof Error) {
+            console.log("%s did not load. Exiting.", url);
+            this.retry(5000);
+        }
+        else
+            return result;
+    });
+    return loadedFile;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -65,8 +79,13 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_to_file>', 'URL to check', clone(assertURLExists), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    if (program.url !== 'http://localhost/') {
+        var checkJson = checkHtmlFile(assertURLExists(program.url), program.checks);
+    } else {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
